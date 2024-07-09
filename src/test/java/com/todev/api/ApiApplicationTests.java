a4fc878c6033ecb56acbd89b972b9883da63f277
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2024-07-09 11:04:28 -0300

    add(project): first commit;

---
+++ src/test/java/com/todev/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
