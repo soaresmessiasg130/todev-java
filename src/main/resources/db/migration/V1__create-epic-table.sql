a05eabf03d543fbda8b07784bdc6daabcb8b24ff
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2024-07-12 08:41:27 -0300

    refact(migrations): create V0 migration, and unify everything on V1;

--- src/main/resources/db/migration/V1__create-epic-table.sql
+++
@@ -1,9 +0,0 @@
-create extension if not exists "pgcrypto";
-
-create table epic (
-  id UUID default gen_random_uuid() primary key,
-  title text null,
-  "desc" text null,
-  status boolean not null,
-  created_at timestamp null
-);
---
+++ src/main/resources/db/migration/V1__create-tables.sql
@@ -0,0 +1,36 @@
+create table person (
+  id UUID default gen_random_uuid() primary key,
+  full_name text null,
+  email text null,
+  phone text null,
+  job text null
+);
+
+create table "user" (
+  id UUID default gen_random_uuid() primary key,
+  login text not null,
+  password text not null,
+  person_id UUID,
+  foreign key (person_id) references person(id) on delete cascade
+);
+
+create table epic (
+  id UUID default gen_random_uuid() primary key,
+  title text null,
+  "desc" text null,
+  status boolean not null,
+  created_at timestamp null,
+  user_id UUID,
+  foreign key (user_id) references "user"(id) on delete cascade
+);
+
+create table task (
+  id UUID default gen_random_uuid() primary key,
+  title text null,
+  "desc" text null,
+  image_url text null,
+  done boolean not null,
+  created_at timestamp null,
+  epic_id UUID,
+  foreign key (epic_id) references epic(id) on delete cascade
+);
